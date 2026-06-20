27571dc44be1cfd719a5ea12aced0652fc02b791
fathima-rinsha-k745 <fathimarinshak745@gmail.com>
2026-06-20 20:15:57 +0530

    Fix Railway ALLOWED_HOSTS

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,29 +1,12 @@
-from django.shortcuts import render
-
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from .serializers import RegisterSerializer
-
-@api_view(['POST'])
-def register(request):
-    serializer = RegisterSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"message": "User registered successfully"})
-
-    return Response(serializer.errors, status=400)
-
-from django.shortcuts import render
 from django.contrib.auth import authenticate
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from .serializers import RegisterSerializer
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def register(request):
     serializer = RegisterSerializer(data=request.data)
 
@@ -35,6 +18,7 @@ def register(request):
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login(request):
     username = request.data.get('username')
     password = request.data.get('password')
